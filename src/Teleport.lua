57ae944b913e19b7dbb84ef20f458385a1df704d
amirulaldy <145275313+amirulaldy@users.noreply.github.com>
2026-02-03 03:32:30 +0800

    Update Teleport.lua

--- src/Teleport.lua
+++ src/Teleport.lua
@@ -1,15 +1,24 @@
 local Teleport = {}
+local plr = game.Players.LocalPlayer
 
 Teleport.Locations = {
-    Jungle = CFrame.new(0,0,0),
-    Volcano = CFrame.new(100,0,100),
+	Jungle = CFrame.new(0,10,0),
+	Ocean = CFrame.new(500,10,200),
+	Volcano = CFrame.new(1000,20,400),
+	CrystalCave = CFrame.new(1500,15,700),
 }
 
 function Teleport:Go(name)
-    local char = game.Players.LocalPlayer.Character
-    if char and self.Locations[name] then
-        char:PivotTo(self.Locations[name])
-    end
+	local char = plr.Character
+	if char and self.Locations[name] then
+		char:PivotTo(self.Locations[name])
+	end
+end
+
+function Teleport:OpenMenu()
+	for name,_ in pairs(self.Locations) do
+		warn("Teleport available:", name)
+	end
 end
 
 return Teleport
