23b577b1c91fc6415a28cb3fb8861e1207208944
amirulaldy <145275313+amirulaldy@users.noreply.github.com>
2026-02-03 03:08:21 +0800

    Update Config.lua

--- src/Config.lua
+++ src/Config.lua
@@ -1,12 +1,25 @@
+local HttpService = game:GetService("HttpService")
+local file = "AmsHub_Config.json"
+
 local Config = {
-	FirstRun = true
+    Mode = "Safe",
+    UIVisible = true
 }
 
-function Config:Load()
-	-- Mobile executor sering tidak support filesystem
-end
-
 function Config:Save()
+    if writefile then
+        writefile(file, HttpService:JSONEncode(self))
+    end
 end
 
+function Config:Load()
+    if readfile and isfile and isfile(file) then
+        local data = HttpService:JSONDecode(readfile(file))
+        for k,v in pairs(data) do
+            self[k] = v
+        end
+    end
+end
+
+Config:Load()
 return Config
