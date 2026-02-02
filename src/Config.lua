6695c56c47e8ca6d7e685a24d9d41e719d9daa5e
amirulaldy <145275313+amirulaldy@users.noreply.github.com>
2026-02-03 03:33:28 +0800

    Update Config.lua

--- src/Config.lua
+++ src/Config.lua
@@ -2,23 +2,22 @@ local HttpService = game:GetService("HttpService")
 local file = "AmsHub_Config.json"
 
 local Config = {
-    Mode = "Safe",
-    UIVisible = true
+	Mode = "Safe"
 }
 
 function Config:Save()
-    if writefile then
-        writefile(file, HttpService:JSONEncode(self))
-    end
+	if writefile then
+		writefile(file, HttpService:JSONEncode(self))
+	end
 end
 
 function Config:Load()
-    if readfile and isfile and isfile(file) then
-        local data = HttpService:JSONDecode(readfile(file))
-        for k,v in pairs(data) do
-            self[k] = v
-        end
-    end
+	if readfile and isfile and isfile(file) then
+		local data = HttpService:JSONDecode(readfile(file))
+		for k,v in pairs(data) do
+			self[k] = v
+		end
+	end
 end
 
 Config:Load()
